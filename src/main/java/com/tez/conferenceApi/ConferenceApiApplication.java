d998d0670c94238e5f5b359a406eaf5d71d0aa11
Pranoy Tez <pranoytez@gmail.com>
2020-10-21 17:57:48 -0500

    Initial Commit | Adding all the files created by Spring Initializr

---
+++ src/main/java/com/tez/conferenceApi/ConferenceApiApplication.java
@@ -0,0 +1,13 @@
+package com.tez.conferenceApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConferenceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tez/conferenceApi/ConferenceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tez.conferenceApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
